9749d0569640677b1f3373d7b3581f1252bef55d
Vincent Thiberville <vthib@pm.me>
2023-02-26 21:28:49 +0100

    fix: properly compile on archs where char is unsigned
    The C type 'char' is not always signed, some archs have it unsigned,
    which makes the code not compile as it is used as a "i8".

--- src/lib.rs
+++ src/lib.rs
@@ -585,7 +585,7 @@ fn byte_array_to_slice(digest: &sys::ByteArray) -> Option<&[u8]> {
 ///
 /// The pointer must be valid at least as long as the borrow used to generate the lifetime of
 /// the slice.
-unsafe fn cstr_ptr_to_slice(ptr: &*mut i8) -> Option<&[u8]> {
+unsafe fn cstr_ptr_to_slice(ptr: &*mut std::ffi::c_char) -> Option<&[u8]> {
     if ptr.is_null() {
         None
     } else {
