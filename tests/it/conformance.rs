39a98b64aad1745a094c04321df809a3a7d7cad7
Vincent Thiberville <vthib@pm.me>
2024-02-13 22:59:39 +0100

    test: add another test from the original repo

--- tests/it/conformance.rs
+++ tests/it/conformance.rs
@@ -28,12 +28,12 @@ mod sys {
     }
 }
 
-fn get_test_data() -> Vec<u8> {
+fn get_test_data(path: &str) -> Vec<u8> {
     use sys::*;
 
     unsafe {
         let bio = BIO_new(BIO_s_mem());
-        let data = std::fs::read("tests/assets/sig.pem").unwrap();
+        let data = std::fs::read(path).unwrap();
         BIO_write(bio, data.as_ptr(), data.len() as _);
         let mut name = std::ptr::null();
         let mut header = std::ptr::null();
@@ -47,7 +47,7 @@ fn get_test_data() -> Vec<u8> {
 
 #[test]
 fn first_signature_content() {
-    let data = get_test_data();
+    let data = get_test_data("tests/assets/sig.pem");
     let token = get_init_token();
     let auth = authenticode_parser::parse(&token, &data).unwrap();
 
@@ -475,4 +475,203 @@ fn pe_file() {
             ]
         );
     }
+}
+
+#[test]
+fn microsoft_first_signature_content() {
+    let data = get_test_data("tests/assets/microsoft.pem");
+    let token = get_init_token();
+    let auth = authenticode_parser::parse(&token, &data).unwrap();
+
+    let signatures = auth.signatures();
+
+    //***********************************//
+    // Check the first signature content //
+    let first_sig = &signatures[0];
+    assert_eq!(first_sig.version(), 1);
+
+    assert_eq!(
+        first_sig.digest().unwrap(),
+        [
+            0xc7, 0xfe, 0xf9, 0x4e, 0x32, 0x9b, 0xd9, 0xb6, 0x6b, 0x28, 0x15, 0x39, 0x26, 0x5f,
+            0x98, 0x93, 0x13, 0x35, 0x6c, 0xbd, 0x9c, 0x34, 0x5d, 0xf9, 0xe6, 0x70, 0xe9, 0xc4,
+            0xb6, 0xe0, 0xed, 0xce
+        ]
+    );
+    assert_eq!(first_sig.digest_alg().unwrap(), b"sha256");
+
+    assert_eq!(first_sig.verify_flags(), Some(AuthenticodeVerify::Valid));
+
+    //****************************//
+    // Check SignerInfo structure //
+    let signer = first_sig.signer().unwrap();
+    assert_eq!(signer.digest_alg().unwrap(), b"sha256");
+
+    assert_eq!(
+        signer.digest().unwrap(),
+        [
+            0x16, 0xef, 0xc5, 0x25, 0x0c, 0x4d, 0x4a, 0x99, 0xa0, 0x0e, 0xd2, 0xad, 0x9a, 0x0e,
+            0x3d, 0x8f, 0xbc, 0x21, 0xda, 0x5b, 0xe9, 0x5a, 0xc3, 0x5a, 0xd3, 0x3b, 0x3d, 0x9c,
+            0x3f, 0x37, 0x19, 0xa1
+        ]
+    );
+    assert_eq!(signer.program_name().unwrap(), b"Procexp");
+
+    //******************************************//
+    // Test all certificates of first signature //
+    let certs = first_sig.certs();
+    assert_eq!(certs.len(), 2);
+
+    //**************************//
+    // Check the 1. certificate //
+    let cert = &certs[0];
+    assert_eq!(
+        cert.sha1().unwrap(),
+        [
+            0x92, 0xd7, 0x19, 0x2a, 0x7c, 0x31, 0x80, 0x91, 0x2f, 0xf8, 0x41, 0x4f, 0x79, 0x09,
+            0x73, 0xa0, 0x5c, 0x28, 0xf8, 0xb0
+        ]
+    );
+
+    assert_eq!(cert.version(), 2);
+    assert_eq!(
+        cert.subject().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/CN=Microsoft Windows Hardware \
+        Compatibility Publisher"
+    );
+    assert_eq!(
+        cert.issuer().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/CN=Microsoft Windows Third Party \
+        Component CA 2012"
+    );
+
+    //**************************//
+    // Check the 2. certificate //
+    let cert = &certs[1];
+    assert_eq!(
+        cert.sha1().unwrap(),
+        [
+            0x77, 0xa1, 0x0e, 0xbf, 0x07, 0x54, 0x27, 0x25, 0x21, 0x8c, 0xd8, 0x3a, 0x01, 0xb5,
+            0x21, 0xc5, 0x7b, 0xc6, 0x7f, 0x73
+        ]
+    );
+
+    //*******************************************//
+    // Check the Counter signature //
+    let countersigs = first_sig.countersigs();
+    assert_eq!(countersigs.len(), 1);
+
+    let countersig = &countersigs[0];
+
+    assert_eq!(
+        countersig.verify_flags(),
+        Some(CounterSignatureVerify::Valid)
+    );
+    assert_eq!(countersig.digest_alg().unwrap(), b"sha256");
+    assert_eq!(countersig.sign_time(), 1629165693);
+    assert_eq!(
+        countersig.digest().unwrap(),
+        [
+            0xed, 0xdf, 0x8a, 0x45, 0x34, 0x0e, 0x16, 0xb3, 0x55, 0x6a, 0x8e, 0x52, 0xb3, 0xfc,
+            0xd2, 0xe7, 0x3c, 0x5c, 0x47, 0xd3, 0x6a, 0xa6, 0x71, 0x4f, 0xfe, 0xef, 0x2c, 0x19,
+            0x60, 0x37, 0x67, 0x6f
+        ]
+    );
+
+    let chain = countersig.certificate_chain();
+    assert_eq!(chain.len(), 2);
+
+    //**************************//
+    // Check the 1. certificate //
+    let cert = &chain[0];
+    assert_eq!(
+        cert.sha1().unwrap(),
+        [
+            0x9a, 0xb3, 0xfa, 0x0a, 0x1a, 0xdb, 0xcf, 0x46, 0xb1, 0xee, 0xce, 0x7b, 0x9f, 0x93,
+            0xe8, 0xa7, 0x75, 0x42, 0xf2, 0x0c
+        ]
+    );
+
+    assert_eq!(
+        cert.sha256().unwrap(),
+        [
+            0x8a, 0xaa, 0x18, 0x95, 0xfb, 0x3c, 0x0d, 0x0e, 0xba, 0x54, 0xec, 0x34, 0x41, 0xec,
+            0xc8, 0xb9, 0xef, 0x18, 0xba, 0x18, 0x13, 0x58, 0xb0, 0x68, 0xe0, 0x66, 0xaa, 0xb6,
+            0xa9, 0x53, 0x0a, 0x32
+        ]
+    );
+
+    assert_eq!(cert.version(), 2);
+    assert_eq!(
+        cert.subject().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/OU=Microsoft Operations Puerto \
+        Rico/OU=Thales TSS ESN:32BD-E3D5-3B1D/CN=Microsoft Time-Stamp Service"
+    );
+    assert_eq!(
+        cert.issuer().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/CN=Microsoft Time-Stamp PCA 2010"
+    );
+    assert_eq!(cert.not_after(), 1649703742);
+    assert_eq!(cert.not_before(), 1610650942);
+    assert_eq!(
+        cert.key().unwrap(),
+        b"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA74ah1Pa5wvcyvYNCy/\
+        YQs1tK8rIGlh1Qq1QFaJmYVXLXykb+m5yCStzmL227wJjsalZX8JA2YcbaZV5Icwm9vAJz8AC/sk/\
+        dsUK3pmDvkhtVI04YDV6otuZCILpQB9Ipcs3d0e1Dl2KKFvdibOk0/0rRxU9l+/\
+        Yxeb5lVTRERLxzI+Rd6Xv5QQYT6Sp2IE0N1vzIFd3yyO773T5XifNgL5lZbtIUnYUVmUBKlVoemO/\
+        54aiFeVBpIG+\
+        YzhDTF7cuHNAzxWIbP1wt4VIqAV9JjuqLMvvBSD56pi8NTKM9fxrERAeaTS2HbfBYfmnRZ27Czjeo0ijQ5DSZGi0Er\
+        vWfKQIDAQAB"
+    );
+    assert_eq!(
+        cert.serial().unwrap(),
+        b"33:00:00:01:62:d0:fe:02:f3:01:e5:cd:49:00:00:00:00:01:62"
+    );
+    assert_eq!(cert.sig_alg().unwrap(), b"sha256WithRSAEncryption");
+    assert_eq!(cert.key_alg().unwrap(), b"rsaEncryption");
+
+    //**************************//
+    // Check the 2. certificate //
+    let cert = &chain[1];
+    assert_eq!(
+        cert.sha1().unwrap(),
+        [
+            0x2a, 0xa7, 0x52, 0xfe, 0x64, 0xc4, 0x9a, 0xbe, 0x82, 0x91, 0x3c, 0x46, 0x35, 0x29,
+            0xcf, 0x10, 0xff, 0x2f, 0x04, 0xee
+        ]
+    );
+
+    assert_eq!(
+        cert.sha256().unwrap(),
+        [
+            0x86, 0xec, 0x11, 0x8d, 0x1e, 0xe6, 0x96, 0x70, 0xa4, 0x6e, 0x2b, 0xe2, 0x9c, 0x4b,
+            0x42, 0x08, 0xbe, 0x04, 0x3e, 0x36, 0x60, 0x0d, 0x4e, 0x1d, 0xd3, 0xf3, 0xd5, 0x15,
+            0xca, 0x11, 0x90, 0x20
+        ]
+    );
+
+    assert_eq!(cert.version(), 2);
+    assert_eq!(
+        cert.subject().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/CN=Microsoft Time-Stamp PCA 2010"
+    );
+    assert_eq!(
+        cert.issuer().unwrap(),
+        b"/C=US/ST=Washington/L=Redmond/O=Microsoft Corporation/CN=Microsoft Root Certificate \
+        Authority 2010"
+    );
+    assert_eq!(cert.not_after(), 1751406415);
+    assert_eq!(cert.not_before(), 1278020215);
+    assert_eq!(
+        cert.key().unwrap(),
+        b"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqR0NvHcRijog7PwTl/\
+        X6f2mUa3RUENWlCgCChfvtfGhLLF/Fw+Vhwna3PmYrW/AVUycEMR9BGxqVHc4JE458YTBZsTBED/\
+        FgiIRUQwzXTbg4CLNC3ZOs1nMwVyaCo0UN0Or1R4HNvyRgMlhgRvJYR4YyhB50YWeRX4FUsc+\
+        TTJLBxKZd0WETbijGGvmGgLvfYfxGwScdJGcSchohiq9LZIlQYrFd/XcfPfBXday9ikJNQFHRD5wGPmd/\
+        9WbAA5ZEfu/QS/\
+        1u5ZrKsajyeioKMfDaTgaRtogINeh4HLDpmc085y9Euqf03GS9pAHBIAmTeM38vMDJRF1eFpwBBU8iTQIDAQAB"
+    );
+    assert_eq!(cert.serial().unwrap(), b"61:09:81:2a:00:00:00:00:00:02");
+    assert_eq!(cert.sig_alg().unwrap(), b"sha256WithRSAEncryption");
+    assert_eq!(cert.key_alg().unwrap(), b"rsaEncryption");
 }
